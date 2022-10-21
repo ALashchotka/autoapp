48bb19a5f426fc21f33637449b25b3acf222ffa1
Anton Lashchotka <a.lashchotka@knomary.com>
2022-10-22 01:36:07 +0300

    Eslint init + fixes

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,16 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const path = require("path");
 
 module.exports = {
+  resolver: {
+    extraNodeModules: {
+      src: path.resolve(__dirname, "src"),
+    },
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: true,
+        inlineRequires: false,
       },
     }),
   },
