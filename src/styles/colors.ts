ccadfe20d61de668d1c79d24087dc3b041da259d
Anton Lashchotka <a.lashchotka@knomary.com>
2022-10-22 02:58:55 +0300

    Change app primary color

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,13 +1,13 @@
 export const DARK_THEME = {
   background: "#1a1a1a",
-  primary: "coral",
+  primary: "#4e85be",
   text: "#FFFFFF",
   itemBackground: "#333333",
 };
 
 export const LIGHT_THEME = {
   background: "#EEEEEE",
-  primary: "coral",
+  primary: "#4e85be",
   text: "#000000",
   itemBackground: "#E0E0E0",
 };
